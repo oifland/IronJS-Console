aaff15841135e9d8832ed5a46852309e3720ce91
Orion Ifland <ifland@gmail.com>
2012-02-29 14:44:12 -0600

    Got the basics in place - a Read-Eval-Print-Loop with a prompt

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new IronJS.Hosting.CSharp.Context();
+
+            Console.WriteLine("Type a JS expression below and press enter. (Blank line to quit.)");
+
+            Console.Write("js-console> ");
+            string command = Console.ReadLine();
+
+            while (command != "" && command != "exit")
+            {
+                Console.WriteLine(context.Execute(command));
+                Console.Write("js-console> ");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
